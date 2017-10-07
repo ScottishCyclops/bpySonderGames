127c5159774bf8304f24a8a535bfc23580805a40
= <scottlandart@gmail.com>
2017-10-07 22:35:14 +0200

    added SgOffsetAction operator updated code

--- sondergames.py
+++ sondergames.py
@@ -29,6 +29,7 @@ bl_info = {
 import bpy
 import sys
 from os.path import abspath, exists, join, sep
+import math
 
 
 def find(name: str, search_paths: iter) -> any:
@@ -57,30 +58,29 @@ from io_scene_fbx import export_fbx_bin
 
 # dict containing the custom properties to export an action sequence
 as_export_kwargs = dict(apply_unit_scale=True,
-            axis_up="Z",
-            axis_forward="Y",
-            object_types={"ARMATURE"},
-            use_mesh_modifiers=False,
-            mesh_smooth_type="FACE",
-            bake_anim_use_nla_strips=False,
-            bake_anim_use_all_actions=False,
-            bake_anim_simplify_factor=0.0,
-            add_leaf_bones=False,
-            use_mesh_edges=False,
-            use_tspace=False)
-
+                        axis_up="Z",
+                        axis_forward="Y",
+                        object_types={"ARMATURE"},
+                        use_mesh_modifiers=False,
+                        mesh_smooth_type="FACE",
+                        bake_anim_use_nla_strips=False,
+                        bake_anim_use_all_actions=False,
+                        bake_anim_simplify_factor=0.0,
+                        add_leaf_bones=False,
+                        use_mesh_edges=False,
+                        use_tspace=False)
 
 
 # dict containing the custom properties to export a skeletal mesh
 sk_export_kwargs = dict(apply_unit_scale=True,
-            axis_up="Z",
-            axis_forward="Y",
-            object_types={"MESH", "ARMATURE"},
-            mesh_smooth_type="FACE",
-            bake_anim=False,
-            add_leaf_bones=False,
-            use_mesh_edges=False,
-            use_tspace=False)
+                        axis_up="Z",
+                        axis_forward="Y",
+                        object_types={"MESH", "ARMATURE"},
+                        mesh_smooth_type="FACE",
+                        bake_anim=False,
+                        add_leaf_bones=False,
+                        use_mesh_edges=False,
+                        use_tspace=False)
 
 
 def export_fbx(operator, context, objects, name, kwargs):
@@ -102,11 +102,12 @@ def export_fbx(operator, context, objects, name, kwargs):
         operator.report({"WARNING"}, "File exists: " + file_name)
     else:
         export_fbx_bin.save_single(operator, context.scene,
-                                    filepath=file_path,
-                                    context_objects=objects,
-                                    **kwargs
-                                    )
-        operator.report({"INFO"}, "File " + "overwritten" if file_exists else "exported" + ": " + file_name)
+                                   filepath=file_path,
+                                   context_objects=objects,
+                                   **kwargs
+                                   )
+        operator.report(
+            {"INFO"}, "File " + ("overwritten" if file_exists else "exported") + ": " + file_name)
 
 
 def export_action_sequence(operator, context, action):
@@ -122,7 +123,8 @@ def export_action_sequence(operator, context, action):
         if not action.name.startswith("AS_"):
             operator.report({"WARNING"}, "Action name should start with 'AS_'")
 
-        export_fbx(operator, context, context.scene.objects, action.name, as_export_kwargs)
+        export_fbx(operator, context, context.scene.objects,
+                   action.name, as_export_kwargs)
     except Exception as e:
         operator.report({"WARNING"}, str(e))
 
@@ -147,7 +149,8 @@ def export_skeletal_mesh(operator, context, objects, name):
             armature = obj
 
     if mesh is None or armature is None:
-        operator.report({"ERROR"}, "Selection does not contain a mesh and armature")
+        operator.report(
+            {"ERROR"}, "Selection does not contain a mesh and armature")
     else:
         try:
             if armature.name != "root":
@@ -156,6 +159,8 @@ def export_skeletal_mesh(operator, context, objects, name):
             export_fbx(operator, context, objects, name, sk_export_kwargs)
         except Exception as e:
             operator.report({"WARNING"}, str(e))
+
+
 '''
 # TODO: remove, not usefull
 class SgExportAllActions(bpy.types.Operator):
@@ -191,21 +196,22 @@ class SgExportCurrentAction(bpy.types.Operator):
     overwrite = bpy.props.BoolProperty(name="overwrite", default=False)
 
     def run(self, context):
-        active = bpy.context.active_object
+        active = context.active_object
         if active is not None:
             if active.type != "ARMATURE":
                 self.report({"WARNING"}, "You may want to select an armature")
 
             if active.animation_data is not None:
                 if active.animation_data.action is not None:
-                    export_action_sequence(self, context, active.animation_data.action)
+                    export_action_sequence(
+                        self, context, active.animation_data.action)
                 else:
-                    self.report({"ERROR"}, "Selected object has no active action")
+                    self.report(
+                        {"ERROR"}, "Selected object has no active action")
             else:
                 self.report({"ERROR"}, "Selected object has no animation data")
         else:
             self.report({"ERROR"}, "No active object")
-
 
     def execute(self, context):
         self.run(context)
@@ -228,6 +234,90 @@ class SgExportSkeletalMesh(bpy.types.Operator):
     def run(self, context):
         objects = context.selected_objects
         export_skeletal_mesh(self, context, objects, self.name)
+
+    def execute(self, context):
+        self.run(context)
+        return {"FINISHED"}
+
+    def invoke(self, context, event):
+        return context.window_manager.invoke_props_dialog(self)
+
+
+class SgOffsetAction(bpy.types.Operator):
+    """Offsets the active action by the given amount of frames"""
+
+    bl_idname = "sg.offset_action"
+    bl_label = "Offset active action"
+    bl_options = {"REGISTER", "UNDO"}
+
+    offset = bpy.props.IntProperty(name="offset", default=0)
+
+    def run(self, context):
+        active = context.active_object
+
+        if active is None:
+            self.report({"ERROR"}, "Missing active object")
+            return
+
+        if active.animation_data is None:
+            self.report({"ERROR"}, "Missing animation data")
+            return
+
+        action = active.animation_data.action
+
+        if action is None:
+            self.report({"ERROR"}, "Missing active action")
+            return
+
+        start, end = action.frame_range
+        # not +1 because first and last frame are technicly the same frames
+        num_frames = int(end - start)
+
+        if num_frames % 2 != 0:
+            self.report({"WARNING"}, "Uneven number of frames")
+
+        offset = 0
+        if self.offset == 0:
+            # by default, offset by the center
+            offset = math.floor(num_frames / 2)
+        else:
+            offset = self.offset % num_frames
+
+        # copy keyframes before
+        for fcurve in action.fcurves:
+            for kf in fcurve.keyframe_points:
+                fcurve.keyframe_points.insert(kf.co.x - num_frames, kf.co.y, {"FAST"})
+            # from doc: `Ensure keyframes are sorted in chronological order
+            # and handles are set correctly`
+            fcurve.update()
+
+        # offset all keys and handles
+        for fcurve in action.fcurves:
+            for kf in fcurve.keyframe_points:
+                kf.co.x += offset
+                kf.handle_left.x += offset
+                kf.handle_right.x += offset
+            fcurve.update()
+
+        # add keys at start and end
+        for fcurve in action.fcurves:
+            # start
+            fcurve.keyframe_points.insert(
+                start, fcurve.evaluate(start), {"FAST"})
+            # end
+            fcurve.keyframe_points.insert(
+                end, fcurve.evaluate(end), {"FAST"})
+            fcurve.update()
+
+        # remove keys not in range
+        for fcurve in action.fcurves:
+            # run backwards because we remove elements
+            for i in range(len(fcurve.keyframe_points), 0, -1):
+                kf = fcurve.keyframe_points[i - 1]
+                # if frame out of range
+                if kf.co.x < start or kf.co.x > end:
+                    fcurve.keyframe_points.remove(kf, True)
+            fcurve.update()
 
     def execute(self, context):
         self.run(context)
@@ -263,9 +353,11 @@ class SgToolsUi(bpy.types.Panel):
         row_export_0.prop(context.scene, "export_path")
         row_export_1_label.label(text="Action Sequence")
         # row_export_1.operator(SgExportAllActions.bl_idname, icon="ACTION", text="Export All")
-        row_export_1.operator(SgExportCurrentAction.bl_idname, icon="ACTION", text="Export Active")
+        row_export_1.operator(SgExportCurrentAction.bl_idname,
+                              icon="ACTION", text="Export Active")
         row_export_2_label.label(text="Skeletal Mesh")
-        row_export_2.operator(SgExportSkeletalMesh.bl_idname, icon="MESH_MONKEY", text="Export Selected")
+        row_export_2.operator(SgExportSkeletalMesh.bl_idname,
+                              icon="MESH_MONKEY", text="Export Selected")
 
         # import box
         self.layout.label(text="Import")
@@ -278,18 +370,21 @@ class SgToolsUi(bpy.types.Panel):
 
 def register():
     bpy.types.Scene.export_path = bpy.props.StringProperty(
-            name="Export path",
-            default=sep,
-            description="Define the export path of FBX files",
-            subtype="DIR_PATH"
+        name="Export path",
+        default=sep,
+        description="Define the export path of FBX files",
+        subtype="DIR_PATH"
     )
+    # bpy.types.Action.offset
     # bpy.utils.register_class(SgExportAllActions)
     bpy.utils.register_class(SgExportCurrentAction)
     bpy.utils.register_class(SgExportSkeletalMesh)
     bpy.utils.register_class(SgToolsUi)
+    bpy.utils.register_class(SgOffsetAction)
 
 
 def unregister():
+    bpy.utils.unregister_class(SgOffsetAction)
     bpy.utils.unregister_class(SgToolsUi)
     bpy.utils.unregister_class(SgExportSkeletalMesh)
     bpy.utils.unregister_class(SgExportCurrentAction)
